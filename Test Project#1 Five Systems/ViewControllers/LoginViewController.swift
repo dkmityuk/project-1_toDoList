5390da6d45c1299fe69413fbeb3905bd415297d7
Lomovsky-Alex <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>
2022-08-03 17:04:44 +0300

    Revert "Revert "Initial Commit""
    This reverts commit a1d9591f326a1a3417a60ee8c86a9acbb480d062.

---
+++ Test Project#1 Five Systems/ViewControllers/LoginViewController.swift
@@ -0,0 +1,38 @@
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var eMailLabel: UILabel!
+ 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var eMailTextField: UITextField!
+ 
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    private func updateNextButtonState(){
+        let nameText = nameTextField.text ?? ""
+        let eMailText = eMailTextField.text ?? ""
+        nextButton.isEnabled = !nameText.isEmpty && !eMailText.isEmpty
+    }
+    
+    @IBAction func textChanged(_ sender: UITextField) {
+        updateNextButtonState()
+    }
+
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        let controller = self.storyboard?.instantiateViewController(withIdentifier: "TaskViewController") as! TaskViewController
+      
+        self.navigationController?.pushViewController(controller, animated: true)
+//        self.present(controller, animated: false, completion: nil)
+    }
+    
+}
+
